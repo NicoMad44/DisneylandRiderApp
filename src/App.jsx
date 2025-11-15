b2e80d8660fc6b66559a0465fa32a3cb7b13a3ac
NicoMad44 <nicolas.madier@gmail.com>
2025-11-15 14:17:53 +0100

    premiere version - mockup Fantasiland

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import { Attraction } from "./components/Attraction/Attraction";
+
+
+function App() {
+  const [disneyData, setDisneyData] = useState(null);
+
+  useEffect(() => {
+    async function fetchDisneyData() {
+      try {
+        const response = await fetch('/api/parks/4/queue_times.json');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setDisneyData(data); // Les données sont en state
+      } catch (error) {
+        console.error('Fetch error:', error);
+      }
+    }
+    fetchDisneyData();
+  }, []);
+
+  if (!disneyData || !disneyData.lands) {
+    return <p>Chargement...</p>;
+  }
+
+  const fantasilandRides = disneyData.lands[2].rides;
+
+  fantasilandRides.forEach(ride =>  {
+
+    ride.parc = "Disneyland";
+    ride.land = "Fantasiland"
+    
+  });
+
+  const testList = fantasilandRides.filter((ride) => (ride.id === 22 || ride.id === 18))
+
+  const fantasilandRidesTimeAscending = [...fantasilandRides].sort((a, b) => b.wait_time - a.wait_time );
+  const fantasilandTimeAscending = fantasilandRidesTimeAscending.map((ride) =>  
+    <Attraction key={ride.id}
+      id={ride.id}
+      name={ride.name}
+      parc={ride.parc}
+      land={ride.land}
+      QTime={ride.wait_time}
+      openStatus={ride.is_open}
+      />
+  )
+
+  const fantasilandRidesTimeDecending = [...testList].sort((a, b) => a.wait_time - b.wait_time );
+  const fantasilandTimeDecending = fantasilandRidesTimeDecending.map((ride) =>  
+    <p key={ride.id}>{ride.id}-{ride.name} -- {ride.wait_time}  Min</p>
+  )
+
+  
+
+
+
+
+  return (
+   <div>
+    <h1>DisneyLand Paris - Temps d'attente live</h1>
+    <main>
+      <div>
+        <h2>Fantasiland - Longest to Shortest</h2>
+        {fantasilandTimeAscending}    
+      </div>
+      {/* <div>
+        <h2>Fantasiland - Shortest to Longest Q</h2>
+        {fantasilandTimeDecending}    
+      </div> */}
+    </main>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Attraction/Attraction.jsx
@@ -0,0 +1,41 @@
+export function Attraction({id, name, parc, land, QTime, openStatus}){
+ 
+    const title = openStatus ? 
+            <h3>{name}</h3> 
+            :
+            <div><h3 className="closed">{id}-{name} - Fermé 😣</h3>  </div>
+
+
+    return(
+
+        <div key={id} className="attraction">
+            <img className="attractionIcon" src={`src/assets/images/${id}_icon.svg`} alt={`"Logo attraction ${name}`} />
+            <div className="attractionInfo">
+                {title} {id}
+                
+                <div className="location">
+                    <p>{parc}</p>
+                    <p>{land}</p>
+                </div>
+            </div>
+            <div className="attractionKpi">
+                {QTime} min
+            </div>
+            
+
+        </div>
+    )
+   
+
+
+
+
+
+
+
+
+
+
+
+
+}
